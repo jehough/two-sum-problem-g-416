0fa3147d570e578b854005e96167a8d47b8a4977
Jim Hough <teachlife@outlook.com>
2020-06-03 22:56:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ function bruteForceTwoSum(arr, sum){
   for (let num of arr){
     for (let num2 of arr){
       if (num + num2 === sum){
+        console.log(num + num2)
         solution.push([num, num2])
       }
       arr.shift()
