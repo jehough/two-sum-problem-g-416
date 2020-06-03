a1eaac39010e35739031bd5e7a8c9c9ca5a26e62
Jim Hough <teachlife@outlook.com>
2020-06-03 23:40:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function bruteForceTwoSum(arr, sum){
 
 function binarySearchTwoSum(arr, sum){
  let solution = []
- arr.sort()
+ mergeSort(arr)
  for(num of arr){
    if (checkSolutions(num, solution)){
      let num2 = sum - num
@@ -26,4 +26,33 @@ function checkSolutions(num, solution){
   let answer = true
   solution.forEach(arr => arr.includes(num) ? answer = false : null )
   return answer
+}
+
+function findMinAndRemoveSorted(array){
+  return array.shift()
+}
+
+function merge(array1 , array2){
+  let sorted = [];
+  while(array1.length > 0 && array2.length > 0){
+    if (array1[0] > array2[0]){
+      sorted.push(findMinAndRemoveSorted(array2))
+    }
+    else{
+      sorted.push(findMinAndRemoveSorted(array1))
+    }
+  }
+  return sorted.concat(array1).concat(array2)
+}
+
+function mergeSort(array){
+  let midpoint = Math.round(array.length/2)
+  let firstHalf = array.slice(0, midpoint)
+  let secondHalf = array.slice(midpoint, array.length)
+  if (array.length < 2){
+    return array
+  }
+  else{
+    return merge(mergeSort(firstHalf), mergeSort(secondHalf))
+  }
 }
