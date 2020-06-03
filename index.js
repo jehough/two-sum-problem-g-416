32db9639256f74395fe5b27e9a2882c409d1852a
Jim Hough <teachlife@outlook.com>
2020-06-03 22:52:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ function bruteForceTwoSum(arr, sum){
     for (let num2 of arr)
       if (num + num2 === sum){
         let solArr = [num, num2]
-        arr = arr.filter(e => e !== num && e !== num2)
         solution.push(solArr)
       }
   }
